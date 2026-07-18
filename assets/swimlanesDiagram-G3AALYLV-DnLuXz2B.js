2bdbdb778e8fe34a44b635333ea52c30643fb5c3
Anton Shyrko <anton.shyrko@gmail.com>
2026-07-18 16:22:17 +0200

    Updates

---
+++ assets/swimlanesDiagram-G3AALYLV-DnLuXz2B.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-23GEKE2U-7kb1OZT3.js";import{_ as a}from"./index-DpCcRh16.js";import"./chunk-5VM5RSS4-Dpj3hCWy.js";import"./chunk-XXDRQBXY-Dsoo7UHN.js";import"./chunk-VR4S4FIN-CCtPXP5h.js";import"./chunk-32BRIVSS-BX1dvWDJ.js";import"./channel-BWbBpBvH.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
